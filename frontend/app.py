4520787678def6ffc4bdb0ce95bd7ed3cea0e28f
Pogbe Emmanuel <mauyonpogbe@gmail.com>
2026-06-01 08:37:50 +0100

    Updated logging configurations

--- frontend/app.py
+++ frontend/app.py
@@ -1,9 +1,14 @@
 import logging
 import py_nuban
+import sys
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+logging.basicConfig(
+    stream=sys.stdout, 
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s'
+)
 logger = logging.getLogger(__name__)
 
 # Check ../demo/test_algorithm.py
